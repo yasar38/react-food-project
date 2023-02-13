38d0a5f7cf51db2f4b84abedd9b6adadffe9174f
yasar.yilmaz <yasar.yilmaz@mubuto.com>
2023-02-13 13:16:26 +0300

    General project infrastructure arrangement.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#00bcd4",
+      }
+    },
   },
   plugins: [],
 }
